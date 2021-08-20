2fc9e64a2d028e1aa3077973e7735c51768cf5bd
Batareika007 <batareika666@gmail.com>
2021-08-20 16:24:11 +0300

    edit game for starting on click
    change script, start game on click and disable double click

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+document.querySelector("h1").onclick = gameProcces;
+
+function gameProcces(){
 let randomNumber1 = Math.round(Math.random() * 5 + 1);
 let randomNumber2 = Math.round(Math.random() * 5 + 1);
 console.log(randomNumber1);
@@ -5,7 +8,6 @@ console.log(randomNumber1);
 document.querySelector(".dicee__image1").setAttribute("src", "img/dice" + randomNumber1 + ".png");
 document.querySelector(".dicee__image2").setAttribute("src", "img/dice" + randomNumber2 + ".png");
 
-function gameProcces(){
     if (randomNumber1 > randomNumber2){
         document.querySelector("h1").textContent = "Player 1 Wins!";
     }
@@ -13,8 +15,11 @@ function gameProcces(){
         document.querySelector("h1").textContent = "Player 2 Wins!";
     }
     else if (randomNumber1 == randomNumber2){
-        document.querySelector("h1").textContent = "It's a Draw, try again";
+        document.querySelector("h1").textContent = "Draw";
     }
 } 
 
-gameProcces();
+// setTimeout(() => {gameProcces() 
+    
+// }, 5000)
+// gameProcces();
